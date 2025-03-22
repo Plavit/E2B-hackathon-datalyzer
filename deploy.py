37d7ff31926372fb9879c260e35a92469fe7281f
Simon Podhajsky <simon.podhajsky@gmail.com>
2025-03-22 16:48:15 +0100

    Add Modal + deployment script

---
+++ deploy.py
@@ -0,0 +1,36 @@
+# Run with `modal serve deploy.py` or deploy stably with `modal deploy deploy.py`
+import modal
+
+from e2b_hackathon.dash_app import app as web_app
+
+
+image = modal.Image.debian_slim().pip_install(
+    [
+        "e2b-code-interpreter",
+        "python-dotenv",
+        "pandas",
+        "dash",
+        "dash-bootstrap-components",
+        "plotly",
+        "structlog",
+        "pyarrow",
+        "fastparquet",
+        "python-docx",
+        "pypdf2",
+        "openai",
+        "openpyxl",
+        "matplotlib",
+        "seaborn",
+    ]
+)
+app = modal.App(image=image)
+
+
+@app.function(
+    image=image,
+    allow_concurrent_inputs=1000,
+    secrets=[modal.Secret.from_name("e2b-hackathon")],
+)
+@modal.wsgi_app()
+def flask_app():
+    return web_app.server
